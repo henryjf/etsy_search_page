ac2b47619f7e2f42161992cbf30a3cdf056debb2
JEFF HENRY <henry_jf@hotmail.com>
2016-04-17 14:18:18 -0400

    added pricing and store names to imageArea

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,15 @@ items.forEach(function(item){
 
   items.forEach(function(item){
     var title= item.title;
-  })
+  });
+
+  items.forEach(function(item){
+    var shop_name = item.Shop.shop_name;
+  });
+// console.log(shop_name);
+  items.forEach(function(item){
+    var price = item.price;
+  });
   // console.log(item.title);
 
 // console.log(items);
@@ -20,8 +28,11 @@ items.forEach(function(item){
   <div class="imageArea">
   <img class="images" src="${item.Images[0].url_170x135}"alt="">
   <h4>${item.title}</h4>
+  <a href="#">${item.Shop.shop_name}</a>
+  <span>${item.price}</span>
   </div>
   `
+  // <a href="#">Jewelry</a>
   // for(var i = 0; i < 16; i ++);
   // return i = imageArea;
   // console.log(imageArea);
