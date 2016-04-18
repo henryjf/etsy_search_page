7b95b0c7388b6d90b1e50e744a07662d4e7325df
JEFF HENRY <henry_jf@hotmail.com>
2016-04-18 07:17:38 -0400

    add Readme

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -9858,14 +9858,11 @@ var _etsyinfo = require('./etsyinfo');
 
 var _etsyinfo2 = _interopRequireDefault(_etsyinfo);
 
-// console.log(etsyImages.results);
-
 var prod = (0, _jquery2['default'])('.products');
 var items = _etsyinfo2['default'].results;
 
 items.forEach(function (item) {
   var image = item.Images[0].url_170x135;
-  // console.log(image);
 
   items.forEach(function (item) {
     var title = item.title;
@@ -9874,19 +9871,13 @@ items.forEach(function (item) {
   items.forEach(function (item) {
     var shop_name = item.Shop.shop_name;
   });
-  // console.log(shop_name);
+
   items.forEach(function (item) {
     var price = item.price;
   });
-  // console.log(item.title);
 
-  // console.log(items);
   var imageArea = '\n  <div class="imageArea">\n  <img class="images" src="' + item.Images[0].url_170x135 + '"alt="">\n  <h4>' + item.title + '</h4>\n  <a href="#">' + item.Shop.shop_name + '</a>\n  <span>' + item.price + '</span>\n  </div>\n  ';
-  // <img class="hamburger" src="{/images/hamburger.png}">
-  // <a href="#">Jewelry</a>
-  // for(var i = 0; i < 16; i ++);
-  // return i = imageArea;
-  // console.log(imageArea);
+
   prod.append(imageArea);
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
 import $ from 'jquery';
 import etsyImages from './etsyinfo';
 
-// console.log(etsyImages.results);
-
 var prod = $('.products');
 var items = etsyImages.results;
 
 items.forEach(function(item){
   var image = item.Images[0].url_170x135;
-  // console.log(image);
 
   items.forEach(function(item){
     var title= item.title;
@@ -17,13 +14,12 @@ items.forEach(function(item){
   items.forEach(function(item){
     var shop_name = item.Shop.shop_name;
   });
-// console.log(shop_name);
+
   items.forEach(function(item){
     var price = item.price;
   });
-  // console.log(item.title);
 
-// console.log(items);
+
   var imageArea = `
   <div class="imageArea">
   <img class="images" src="${item.Images[0].url_170x135}"alt="">
@@ -32,10 +28,6 @@ items.forEach(function(item){
   <span>${item.price}</span>
   </div>
   `
-  // <img class="hamburger" src="{/images/hamburger.png}">
-  // <a href="#">Jewelry</a>
-  // for(var i = 0; i < 16; i ++);
-  // return i = imageArea;
-  // console.log(imageArea);
+
   prod.append(imageArea);
  });
